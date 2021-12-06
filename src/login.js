6b71febc897abb3c615da63ef38b6978548ba336
Brand28 <alejandrobrand28@gmail.com>
2021-12-05 19:20:51 -0500

    ya hay login

--- src/login.js
+++ src/login.js
@@ -2,4 +2,33 @@ import{initializeApp} from "firebase/app";
 import {getFirebaseConfig} from "./firebase-config";
 import{getAuth, signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth";
 
-console.log('sigue funcionando el js o voy a llorar, esto no es un meme');
+const firebaseAppConfig = getFirebaseConfig();
+const firebaseApp = initializeApp(firebaseAppConfig);
+const auth = getAuth();
+
+// =================================================================================== //
+
+function iniciarSesion(e, event){
+    signInWithEmailAndPassword(auth, usuario.value, contraseña.value)
+    .then((userCredentials) => {
+        console.log(userCredentials);
+
+        window.location.href = "main.html";
+    }).catch((error) => {
+        console.log(error.message);
+    });
+}
+
+// =================================================================================== //
+
+const usuario = document.getElementById("in_usuario");
+const contraseña = document.getElementById("in_contraseña");
+const iniSes = document.getElementById("bn_iniSes");
+
+// =================================================================================== //
+
+function aviso(e, event){
+    console.log('hubo click');
+}
+
+iniSes.addEventListener("click", iniciarSesion);
